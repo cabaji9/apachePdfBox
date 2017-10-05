d2830009563eb851f294a4cb96f8ee538c137270
Hyun woo son lee <hson@advlatam.com>
2017-10-05 16:23:28 -0500

    Inital commit

---
+++ README.md
@@ -0,0 +1,10 @@
+##SIMPLE USAGE OF GHOSTSCRIPT TO TRANSFORM PDF TO IMAGE
+
+This example uses apachepdfbox to transform pdf to image.
+After its transformed it used awt to add watermark to the image.
+
+
+
+Add a pdf 
+
+Just run it!.
---
+++ src/main/java/com/test/image/testImage.java
@@ -0,0 +1,115 @@
+package com.test.image;
+
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Hyun Woo Son on 9/12/17.
+ */
+public class testImage {
+
+
+    public static void main(String args[]) throws Exception
+    {
+        String text = "SUPER PRUEBA";
+
+        PDDocument document = PDDocument.load(new File("/Users/User/Downloads/PagareOrdenTasaFijaD201708020901086465.pdf"));
+        PDFRenderer renderer = new PDFRenderer(document);
+
+        int numberOfPages =document.getNumberOfPages();
+        float resolution = 100f;
+
+        List<Image> images =   obtainAllImagePages(numberOfPages,renderer,resolution);
+        List<BufferedImage> imagesWaterMarked = new ArrayList<>();
+
+        for (int i = 0; i < images.size(); i++) {
+
+            Image image =    images.get(i);
+            BufferedImage bufferedImage= addWaterMark(image,text);
+            imagesWaterMarked.add(bufferedImage);
+            ImageIO.write(bufferedImage, "png", new File((i + 1) + ".png"));
+
+        }
+
+        BufferedImage concatenatedImage = null;
+        for (int i = 0; i < imagesWaterMarked.size(); i++) {
+            if(i == 0){
+                concatenatedImage = imagesWaterMarked.get(i);
+            }
+            else{
+                concatenatedImage =concatenateImages(concatenatedImage,imagesWaterMarked.get(i));
+            }
+        }
+        ImageIO.write(concatenatedImage, "png", new File( "final.png"));
+    }
+
+
+    private static List<Image> obtainAllImagePages(int numberOfPages,PDFRenderer pdfRenderer,float dpi) throws Exception{
+        List<Image> images = new ArrayList<>();
+        for(int n=0;n<numberOfPages;n++){
+            BufferedImage image = pdfRenderer.renderImageWithDPI(n,dpi);
+            images.add(image);
+        }
+        return images;
+    }
+
+
+    private static BufferedImage addWaterMark(Image image,String text){
+
+        // create BufferedImage object of same width and height as of original image
+        BufferedImage bufferedImage = new BufferedImage(image.getWidth(null),
+                image.getHeight(null), BufferedImage.TYPE_INT_RGB);
+
+        Integer fontSize = 200;
+
+        Graphics graphics = bufferedImage.getGraphics();
+        graphics.drawImage(image, 0, 0, null);
+        graphics.setColor(new Color(192,192,192,80));
+        // set font for the watermark text
+        graphics.setFont(rotateFont(fontSize));
+
+        // add the watermark text
+        int y= -100;
+        int x= 0;
+        graphics.drawString(text, x, y);
+        graphics.drawString(text, x, y+=fontSize*2+50);
+        graphics.drawString(text, x, y+=fontSize*2+50);
+        graphics.drawString(text, x, y+=fontSize*2+50);
+        graphics.dispose();
+
+        return bufferedImage;
+
+    }
+
+
+    private static Font rotateFont(int fontSize){
+        Font font = new Font("Arial", Font.BOLD, fontSize);
+        AffineTransform affineTransform = new AffineTransform();
+        affineTransform.rotate(Math.toRadians(45), 0, 0);
+        return  font.deriveFont(affineTransform);
+
+    }
+
+
+    private static BufferedImage concatenateImages(BufferedImage parent, BufferedImage toAdd){
+
+        BufferedImage img = new BufferedImage(
+                parent.getWidth(),
+                parent.getHeight()+toAdd.getHeight(), // addition of widths and heights of the images we already have
+                BufferedImage.TYPE_INT_RGB);
+
+        img.createGraphics().drawImage(parent, 0, 0, null); // 0, 0 are the x and y positions
+        img.createGraphics().drawImage(toAdd, 0, parent.getHeight(), null); // here width is mentioned as width of
+        return img;
+    }
+}
